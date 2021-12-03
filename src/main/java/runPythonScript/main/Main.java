7e0427aea9849f5b5d09660cea9b7b3bbe161c7d
René Kwee <rsh.kwee@gmail.com>
2021-12-03 16:14:32 +0100

    Puzzelen.....

--- src/main/java/runPythonScript/main/Main.java
+++ src/main/java/runPythonScript/main/Main.java
@@ -1,9 +1,13 @@
 package runPythonScript.main;
 
 import java.io.StringWriter;
+import java.util.logging.Logger;
+
 import org.python.util.PythonInterpreter;
 
 public class Main {
+  private static final Logger LOGGER = Logger.getLogger(Class.class.getName());
+
   public static void main(String[] args) {
     String a_Script = args[0];
     PythonInterpreter.initialize(System.getProperties(), System.getProperties(), args);
@@ -12,6 +16,6 @@ public class Main {
     python.setOut(out);
     python.execfile(a_Script);
     String outputStr = out.toString();
-    System.out.println(outputStr);
+    LOGGER.info(outputStr);
   }
 }
